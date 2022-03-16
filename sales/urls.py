e5ecfab6b9b4c7a2143a8645c7af6c07c66ae238
AyushiNamdev <ayushinamdev2@gmail.com>
2022-03-16 06:15:04 -0400

    changes in the file

---
+++ sales/urls.py
@@ -0,0 +1,17 @@
+import imp
+from turtle import home
+from django.urls import path
+from .views import (
+    home_view,
+    SaleListView,
+    SaleDetailView,
+)
+
+
+app_name = 'sales'
+
+urlpatterns = [
+    path('', home_view, name = 'home'),
+    path('sales/', SaleListView.as_view(), home_view, name = 'list'),
+    path('sales/<pk>/', SaleDetailView.as_view(), name = 'detail'),
+]
